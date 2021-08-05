8362b61f43041bd26adf5cf0eecff0fd03055567
Kushagra Bainsla <kushagrabainsla@gmail.com>
2021-08-05 17:35:35 +0530

    Initialised play-frontend

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    extends: ['airbnb-base', 'plugin:react/recommended'],
+    parser: 'babel-eslint',
+    rules: {
+        indent: 'off',
+        'react/react-in-jsx-scope': 'off',
+    },
+};
--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
         <div className='App'>
             Home
         </div>
-    )
+    );
 }
 
 export default App;
